5b106d8faf887a4630bfa75cfff83845b33dcbcc
Thomas <jtruant@gmx.com>
2016-01-12 22:34:11 -0700

    basic app

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
 if __name__ == "__main__":
     user_agent = "Urban-slack/1.0 (https://github.com/t3h2mas/urban-slack; t.homas.noee@gmail.com)"
 
+    app.run()
