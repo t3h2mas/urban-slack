071c13296ac07786326d0f96d53c37da7bffc4f0
Thomas <jtruant@gmx.com>
2016-01-13 23:05:51 -0700

    json response

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import urban
 app = Flask(__name__)
+
+def craft_response(results):
+    r = {}
+    r['text'] = results[0]
+    r['attachments'] = [{'text': results[1]}]
+    return r
 
 @app.route('/')
 def hello_world():
     word = request.args.get("text")
     results = urban.lookup(word)
-    return "\n".join(results)
+    return jsonify(craft_response(results))
 
 if __name__ == "__main__":
     app.run()
