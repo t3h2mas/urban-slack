39847c492896a798134862fb22ab0172a658e64e
Thomas <jtruant@gmx.com>
2016-01-13 22:48:10 -0700

    changed to GET method

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 from flask import Flask, request
 app = Flask(__name__)
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/')
 def hello_world():
-    if request.method == 'POST':
-        word = request.data
-        return "looking up %s" % word
-    return 'Hello World!'
+    return "TARGETING " + request.args.get("text")
 
 if __name__ == "__main__":
     user_agent = "Urban-slack/1.0 (https://github.com/t3h2mas/urban-slack; t.homas.noee@gmail.com)"
