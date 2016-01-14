10c416fa81deb70d5d4f39b9e5c9bef67432446b
Thomas <jtruant@gmx.com>
2016-01-13 20:10:40 -0700

    port

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 @app.route('/', methods=['POST', 'GET'])
 def hello_world():
     if request.method == 'POST':
-        word = request.args.get('search', '')
+        word = request.args.get('text', '')
         return "looking up {}".format(word)
     return 'Hello World!'
 
 if __name__ == "__main__":
     user_agent = "Urban-slack/1.0 (https://github.com/t3h2mas/urban-slack; t.homas.noee@gmail.com)"
 
-    app.run()
+    app.run(port=80)
